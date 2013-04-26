b29de49e11c82ad1d90cd92136bc943f09073ee3
frank <fmather@maxmind.com>
2013-04-26 17:05:37 -0400

    add more traits fields

--- src/main/java/com/maxmind/geoip2/TraitsRecord.java
+++ src/main/java/com/maxmind/geoip2/TraitsRecord.java
@@ -8,11 +8,42 @@ import org.json.*;
 public class TraitsRecord
 {
   private String ipAddress;
+  private int autonomousSystemNumber;
+  private String autonomousSystemOrganization;
+  private String domain;
+  private String isp;
+  private String organization;
+  private String userType;
+  
   public TraitsRecord(JSONObject json) throws JSONException {
     ipAddress = json.getString("ip_address");
+      autonomousSystemNumber = json.optInt("autonomous_system_number");
+      domain = json.optString("domain");
+      isp = json.optString("isp");
+      organization = json.optString("organization");
+      userType = json.optString("user_type");
   }
   public String getIpAddress() {
     return ipAddress;
   }
+  public int getAutonomousSystemNumber() {
+    return autonomousSystemNumber;
+  }
+  public String getAutonomousSystemOrganization() {
+    return autonomousSystemOrganization;
+  }
+  public String getDomain() {
+    return domain;
+  }
+  public String getIsp() {
+    return isp;
+  }
+  public String getOrganization() {
+    return organization;
+  }
+  public String getUserType() {
+    return userType;
+  }
 }
+
 
