2261d209e4225d6b7293f9074f4ecbd6223847fa
frank <fmather@maxmind.com>
2013-05-03 11:45:35 -0400

    add is_anonymous_proxy

--- src/main/java/com/maxmind/geoip2/TraitsRecord.java
+++ src/main/java/com/maxmind/geoip2/TraitsRecord.java
@@ -14,19 +14,24 @@ public class TraitsRecord
   private String isp;
   private String organization;
   private String userType;
+  private boolean anonymousProxy;
+  private boolean transparentProxy;
+  private boolean usMilitary;
   
   public TraitsRecord(JSONObject json) throws JSONException {
     ipAddress = json.getString("ip_address");
     if (json.has("autonomous_system_number")) {
       autonomousSystemNumber = new Integer(json.getInt("autonomous_system_number"));
     }
-    if (json.has("autonomous_system_organization")) {
-      autonomousSystemOrganization = new Integer(json.getInt("autonomous_system_organization"));
-    }
+    autonomousSystemOrganization = json.optString("autonomous_system_organization",null);
     domain = json.optString("domain",null);
     isp = json.optString("isp",null);
     organization = json.optString("organization",null);
     userType = json.optString("user_type",null);
+    anonymousProxy = json.optBoolean("is_anonymous_proxy");
+    transparentProxy = json.optBoolean("is_transparent_proxy");
+    usMilitary = json.optBoolean("is_us_military");
+
   }
   public String getIpAddress() {
     return ipAddress;
@@ -49,6 +54,16 @@ public class TraitsRecord
   public String getUserType() {
     return userType;
   }
+  public boolean isAnonymousProxy() {
+    return anonymousProxy;
+  }
+  public boolean isTransparentProxy() {
+    return transparentProxy;
+  }
+  public boolean isUsMilitary() {
+    return usMilitary;
+  }
+
 }
 
 
