b9f51ff769a003a33924266c21d3843a2a27f83f
Gregory Oschwald <goschwald@maxmind.com>
2016-06-20 07:13:05 -0700

    Prepare for 2.8.0-rc.1

--- dev-bin/release.sh
+++ dev-bin/release.sh
@@ -5,7 +5,7 @@ set -e
 VERSION=$(perl -MFile::Slurp::Tiny=read_file -MDateTime <<EOF
 use v5.16;
 my \$log = read_file(q{CHANGELOG.md});
-\$log =~ /\n(\d+\.\d+\.\d+) \((\d{4}-\d{2}-\d{2})\)\n/;
+\$log =~ /\n(\d+\.\d+\.\d+(?:-[\d\w\.]+)) \((\d{4}-\d{2}-\d{2})\)\n/;
 die 'Release time is not today!' unless DateTime->now->ymd eq \$2;
 say \$1;
 EOF
