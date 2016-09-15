fd4a6838f31bd9d938588d079919cc5f6c3780d9
Gregory Oschwald <goschwald@maxmind.com>
2016-09-15 15:38:39 -0700

    Fix typo in release script regex

--- dev-bin/release.sh
+++ dev-bin/release.sh
@@ -5,8 +5,8 @@ set -e
 VERSION=$(perl -MFile::Slurp::Tiny=read_file -MDateTime <<EOF
 use v5.16;
 my \$log = read_file(q{CHANGELOG.md});
-\$log =~ /\n(\d+\.\d+\.\d+(?:-[\w\.\-]+)) \((\d{4}-\d{2}-\d{2})\)\n/;
-die 'Release time is not today!' unless DateTime->now->ymd eq \$2;
+\$log =~ /\n(\d+\.\d+\.\d+(?:-[\w\.\-]+)?) \((\d{4}-\d{2}-\d{2})\)\n/;
+die "Release time (\$2) is not today!" unless DateTime->now->ymd eq \$2;
 say \$1;
 EOF
 )
