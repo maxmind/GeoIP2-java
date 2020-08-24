6cbf8327eb7340eb452997fa96f28d7c9f016025
William Storey <wstorey@maxmind.com>
2020-08-24 10:53:19 -0700

    Use original classes for ISP deserializing

--- src/main/java/com/maxmind/geoip2/model/IspResponse.java
+++ src/main/java/com/maxmind/geoip2/model/IspResponse.java
@@ -3,6 +3,8 @@ package com.maxmind.geoip2.model;
 import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.maxmind.db.MaxMindDbConstructor;
+import com.maxmind.db.MaxMindDbParameter;
 import com.maxmind.db.Network;
 import com.maxmind.geoip2.NetworkDeserializer;
 
@@ -41,17 +43,36 @@ public class IspResponse extends AsnResponse {
         this.organization = organization;
     }
 
+    @MaxMindDbConstructor
     public IspResponse(
-            IspDatabaseModel model,
+            @MaxMindDbParameter(name="autonomous_system_number") Long autonomousSystemNumber,
+            @MaxMindDbParameter(name="autonomous_system_organization") String autonomousSystemOrganization,
+            @MaxMindDbParameter(name="ip_address") String ipAddress,
+            @MaxMindDbParameter(name="isp") String isp,
+            @MaxMindDbParameter(name="organization") String organization,
+            @MaxMindDbParameter(name="network") Network network
+    ) {
+        this(
+            autonomousSystemNumber != null ? autonomousSystemNumber.intValue() : null,
+            autonomousSystemOrganization,
+            ipAddress,
+            isp,
+            organization,
+            network
+        );
+    }
+
+    public IspResponse(
+            IspResponse response,
             String ipAddress,
             Network network
     ) {
         this(
-            model.getAutonomousSystemNumber() != null ? model.getAutonomousSystemNumber().intValue() : null,
-            model.getAutonomousSystemOrganization(),
+            response.getAutonomousSystemNumber(),
+            response.getAutonomousSystemOrganization(),
             ipAddress,
-            model.getIsp(),
-            model.getOrganization(),
+            response.getIsp(),
+            response.getOrganization(),
             network
         );
     }
